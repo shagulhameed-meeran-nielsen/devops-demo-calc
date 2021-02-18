82a84575f394532d6161c7cac3ce791f353c20d1
hash7010 <hash7010@INLC0VKLH2.enterprisenet.org>
2021-02-18 16:06:14 +0530

    Updated the add logic

--- devops-demo/src/main/java/com/demo/test/calc/services/core/AddComponent.java
+++ devops-demo/src/main/java/com/demo/test/calc/services/core/AddComponent.java
@@ -5,7 +5,7 @@ import com.demo.test.calc.models.CalcRequest;
 public class AddComponent {
 	
 	public static Integer add(CalcRequest request) {
-		if(request.getInput1() != null && request.getInput2() != null) {
+		if(request.getInput1() != null && request.getInput1() > 0 && request.getInput2() != null) {
 			return request.getInput1().intValue() + request.getInput2().intValue();
 		} else {
 			return 0;
